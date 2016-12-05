846da6b54465d0c240cec55ec87ceb39abd17cae
Jean-Marc Leoni <jeanmarc.leoni@axa.com>
2016-12-05 14:58:29 +0100

    Making the code just work

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var moment = require('moment');
+var bodyParser = require('body-parser');
+
+app.set('json spaces', 2);
+app.use(bodyParser.json());
+
 
 const calculate = function(req) {
 
   const departureDate = moment(req.body.departureDate, 'YYYY-MM-DD');
   const returnDate = moment(req.body.returnDate, 'YYYY-MM-DD');
-  const nbDays = departureDate.diff(returnDate, 'days');
+  const nbDays = returnDate.diff(departureDate, 'days');
   const rate = 1.8;
-
   console.log(rate * nbDays * req.body.travellerAges.length);
 
   return { quote: rate * nbDays * req.body.travellerAges.length };
