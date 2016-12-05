fb1206557e53fcb217e3c8e45cd5ae6f178dce58
Franck Gass <fgs@fgsMacPro.local>
2016-12-05 23:28:12 +0900

    improve

--- index.js
+++ index.js
@@ -6,23 +6,28 @@ var bodyParser = require('body-parser');
 app.set('json spaces', 2);
 app.use(bodyParser.json());
 
+const RATE = 1.8;
 
-const calculate = function(req) {
+const calculate = function(req, res) {
 
+  //Parse data
   const departureDate = moment(req.body.departureDate, 'YYYY-MM-DD');
   const returnDate = moment(req.body.returnDate, 'YYYY-MM-DD');
-  const nbDays = returnDate.diff(departureDate, 'days') + 1;
-  const rate = 1.8;
-  console.log(rate * nbDays * req.body.travellerAges.length);
 
-  return { quote: rate * nbDays * req.body.travellerAges.length };
+  const nbDays = returnDate.diff(departureDate, 'days');
+
+  if(nbDays < 0) {
+    res.status(400);
+    return {};
+  } else {
+    res.status(200);
+    return { quote: RATE * nbDays * req.body.travellerAges.length };
+  }
 };
 
 app.post('/quote', function (req, res) {
   console.log(req.body);
-
-  res.status(200);
-  res.json(calculate(req));
+  res.json(calculate(req, res));
 });
 
 app.post('/feedback', function (req, res) {
