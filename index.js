e5b815dbb460076a5109eef0867e0a310550c58b
Franck Gass <fgs@fgsMacPro.local>
2016-12-05 22:39:47 +0900

    wip

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var app = express();
-var moment = require('momentjs');
+var moment = require('moment');
 
-app.post('/quote', function (req, res) {
-  console.log(req.body);
+const calculate = function(req) {
 
   const departureDate = moment(req.body.departureDate, 'YYYY-MM-DD');
   const returnDate = moment(req.body.returnDate, 'YYYY-MM-DD');
   const nbDays = departureDate.diff(returnDate, 'days');
   const rate = 1.8;
 
+  return rate * nbDays * req.body.travellerAges.length;
+};
+
+app.post('/quote', function (req, res) {
+  console.log(req.body);
+
   res.status(200);
-  res.json({ quote: rate * nbDays * req.body.travellerAges.length });
-})
+  res.json(calculate(req));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
