2d26e9d4794df55a5fea3cb991ae25878206bd49
Lorenz Hänggi <lolo8304@gmail.com>
2016-12-06 14:50:26 +0100

    undefined in test case

--- test/100-roman.js
+++ test/100-roman.js
@@ -6,13 +6,20 @@ var RomanNumberCalculate = require('../nbr-calculator.js')();
 
     var romans = [
                 { days: 1, value: 1 },
-                { days: 3, value: 3*1 }
+                { days: 3, value: 3*1 },
+                { days: 4, value: 4.4-1 },
+                { days: 5, value: 4.4 },
+                { days: 7, value: 4.4 + 2*1 },
+                { days: 9, value: 8.4 - 1 },
+                { days: 14, value: 8.4 + 4.4 - 1 },
+                { days: 39, value: 3 * 8.4 + 8.4 - 1},
+                { days: 41, value: 39 - 8.4 + 1},
+                { days: 77, value: 39 +2 * 8.4 + 4.4 + 2* 1 }
     ];
 
     for (var i = 0; i < romans.length; i++) {
 
         describe('POST /roman calculator', function () {
-
                 it('test '+i, function (done) {
                     console.log(romans[i]);
                     var IN = romans.indexOf(i).days;
