2a4f2bcb5350c88c14454357a6112e526b58b83e
Jean-Marc Leoni <jeanmarc.leoni@axa.com>
2016-12-06 15:07:32 +0100

    Implemnetion of the romanizer

--- nbr-calculator.js
+++ nbr-calculator.js
@@ -1,21 +1,23 @@
 module.exports = NumberCalc;
-
+I = 1;
+V = 4.2;
+X = 8.4;
+L = 42;
 function NumberCalc() {
 
-    function isNumeric(obj) {
-        // parseFloat NaNs numeric-cast false positives (null|true|false|"")
-        // ...but misinterprets leading-number strings, particularly hex literals ("0x...")
-        // subtraction forces infinities to NaN
-        // adding 1 corrects loss of precision from parseFloat (#15100)
-        return !Array.isArray(obj) && (obj - parseFloat(obj) + 1) >= 0;
-    }
-    function isInteger(obj) {
-        return isNumeric(obj) && obj.indexOf('.') < 0;
+    function romanize (num) {
+        if (!+num)
+            return false;
+        var digits = String(+num).split(""),
+            key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                   0,X,X*2,X*3,L-X,L,L+X,L+X*2,L+X*3,"XC",
+                   0,I,I*2,I*3,V-I,V,V+I,V+2*I,V+3*I,X-I],
+            roman = 0,
+            i = 3;
+        while (i--)
+            roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+        return  parseFloat(Number(parseFloat(roman)).toFixed(1));
     }
 
-    function calcNumber(values) {
-        return values;
-    }
-
-    return calcNumber;
+    return romanize;
 }
