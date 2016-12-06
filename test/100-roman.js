8b5b25472012622e98767ea1a05f20c9739c3161
Lorenz Hänggi <lolo8304@gmail.com>
2016-12-06 14:22:37 +0100

    TEST TDD  - 1st trey

--- test/100-roman.js
+++ test/100-roman.js
@@ -2,21 +2,15 @@ var sinon = require('sinon');
 var chai = require('chai');
 chai.use(require('sinon-chai'));
 var expect = chai.expect;
-var quoteObject = require('../lib/quote-calculator')();
+var RomanNumberCalculate = require('../nbr-calculator.js')();
 
 
-describe('run quote feature', function () {
-  it('calls quote for PL, some days, 2 person', function (done) {
-      var object = {
-            "country":"PL",
-            "departureDate":"2016-11-15",
-            "returnDate":"2016-12-09",
-            "travellerAges":[32,39],
-            "options":["MedicalConditions"],
-            "cover":"Basic"
-            };
-      var quote = quoteObject(object);
-      expect(quote).to.be.equal(192.96)
+describe('POST /roman calculator', function () {
+  it('test 1', function (done) {
+      var IN = 1;
+      var OUT = 1;
+      var romanPrice = RomanNumberCalculate(IN);
+      expect(countryQuote).to.be.equal(OUT)
       done()
   })
 })
