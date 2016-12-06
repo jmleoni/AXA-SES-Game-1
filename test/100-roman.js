39c9b2f7013db74097b7c31b2b988f79b19144a9
Lorenz Hänggi <lolo8304@gmail.com>
2016-12-06 14:47:05 +0100

    first test cases

--- test/100-roman.js
+++ test/100-roman.js
@@ -4,13 +4,23 @@ chai.use(require('sinon-chai'));
 var expect = chai.expect;
 var RomanNumberCalculate = require('../nbr-calculator.js')();
 
+    var romans = [
+                { days: 1, value: 1 },
+                { days: 3, value: 3*1 }
+    ];
 
-describe('POST /roman calculator', function () {
-  it('test 1', function (done) {
-      var IN = 1;
-      var OUT = 1;
-      var romanPrice = RomanNumberCalculate(IN);
-      expect(romanPrice).to.be.equal(OUT)
-      done()
-  })
-})
+    for (var i = 0; i < romans.length; i++) {
+
+        describe('POST /roman calculator', function () {
+
+                it('test '+i, function (done) {
+                    console.log(romans[i]);
+                    var IN = romans.indexOf(i).days;
+                    var OUT = romans.indexOf(i).value;
+                    console.log("run test "+i+" IN="+IN+", OUT="+OUT);
+                    var romanPrice = RomanNumberCalculate(IN);
+                    expect(romanPrice).to.be.equal(OUT)
+                    done()
+                })
+        })
+    }    
